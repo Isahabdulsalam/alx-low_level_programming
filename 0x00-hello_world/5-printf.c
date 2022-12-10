1bacac1faba51432ffdc8dd600f0e9366d59a070
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2022-12-10 08:14:20 -0800

    PRINT F

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - main function 
- *
- * 
- * Return: return 0 everytime
- */
+*main - Main function
+*
+*
+*Return: return 0 everytime
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
