d1cf2c17c71ca5a6399d386e67593974e9ac365e
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2022-12-11 07:21:09 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Alphabet
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= "z"; ch++
+			{write(1, &ch, 1);
+}
+			return (0);
+}
