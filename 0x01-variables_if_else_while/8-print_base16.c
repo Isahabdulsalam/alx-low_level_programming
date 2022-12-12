fb16ce0fcca819c7a128809e5e24d6f4f32189df
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2022-12-12 10:47:11 -0800

    BASED NUMBER

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print hexadecimal numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int m;
+	char B;
+
+	for (m = 0; m < 10; m++)
+		putchar((m % 10) + '0');
+
+	for (B = 'a'; B = 'z'; B++)
+		putchar(B);
+
+	putchar('\n');
+
+	return (0);
+
