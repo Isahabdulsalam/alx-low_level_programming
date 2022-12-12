f87cad6d88e22c3c59fe4e9a5b831d46b9cb15b7
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2022-12-12 08:41:55 -0800

    testing

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <time.h>
+
+/**
+ * main - Lower and Upper Letter
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar("\n");
+
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar("\n\n")
+
+			return (0);
+}
