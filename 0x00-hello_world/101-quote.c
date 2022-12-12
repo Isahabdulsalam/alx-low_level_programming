a60087faa25220474e2ab466849f3bf8ba97aee6
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2022-12-12 13:52:25 -0800

    QUOTE

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio>
+#include <unistd>
+
+/**
+ * main - print the code followed by a new line, to the standard error
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write ("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	return 1
+}
