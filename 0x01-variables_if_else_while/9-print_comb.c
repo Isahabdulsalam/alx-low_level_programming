de8bd36e9144b1186ffcc01cdcfa7645db851c1d
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2022-12-12 11:19:21 -0800

    PRINT COMBINATIONS

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <time.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int B;
+
+	for (B = 0; B <= 9; B++)
+	{
+		putchar((B % 10) + 0);
+		if (B == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchart('\n');
+
+	return (0);
+}
