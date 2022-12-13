0199797efd669c7df4d1e45dceb5a75af12d2120
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2022-12-13 03:14:09 -0800

    QUOTE

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - print the code followed by a new line, to the standard error
+ * main - Prints "and that piece of art is useful" - Dora Korpar,
  *
- * Return: 1
+ * Return: Always 1.
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
 	return (1);
 }
