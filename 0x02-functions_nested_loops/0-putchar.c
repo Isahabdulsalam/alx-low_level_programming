d911a04f95e7faff0d37dfb36e616a5bd5158303
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2022-12-13 04:49:14 -0800

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+#include "Isah.h"
+#include <unistd.h>
 
 /**
- * main - print ISAH followed by new line
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: always 0
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	write
-		("_putchar\n")
-
-	return (0);
+	return (write(1, &c, 1));
 }
