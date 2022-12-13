35ee04a5bc0a85ef504c6ff28b585f7e6e795b93
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2022-12-13 04:36:19 -0800

    Isah

---
+++ 0x02-functions_nested_loops/Isah.h
@@ -0,0 +1,17 @@
+#include <Isah.h>
+
+/**
+ * main - print Isah followed by new line
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('I');
+	_putchar('s');
+	_putchar('a');
+	_putchar('h');
+	_putchar('\n');
+
+	return (0);
+}
