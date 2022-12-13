ccf8791e62ab7d015e39e03bf8d48c92344b38e3
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2022-12-13 03:55:46 -0800

    LOOPS

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <ISAH.h>
+
+/**
+ * main - print ISAH followed by new line
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('I');
+	_putchar('S');
+	_purchar('A');
+	_putchar('\n');
+
+	return (0);
+}
