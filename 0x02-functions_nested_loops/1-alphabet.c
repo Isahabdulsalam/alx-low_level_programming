72457beb5a701d79c937e278eba039b1230b7a74
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2022-12-14 11:27:41 -0800

    lower case with _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print alphabet in lower case using _putchar
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+
+	return (0);
+}
