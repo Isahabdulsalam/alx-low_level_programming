294ec58dd95913c8f02a611bf3fd2c452b271169
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2022-12-17 06:08:35 -0800

    raw

--- 0x03-debugging/3-print_numbers.c
+++ 0x03-debugging/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers from 0-9.
- * Return: void
+ *print_numbers - Prints the numbers from 0-9.
+ *Return: void
  */
 void print_numbers(void)
 {
