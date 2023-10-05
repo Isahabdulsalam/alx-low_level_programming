d16631deb57ae6fb8a4567255a7dcb64e47e13d2
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2023-10-05 14:38:25 -0700

    low level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-
-int main (void)
+/**
+ * main - main point
+ *
+ * puts - print the following text followed new line
+ *
+ * Return: success(0)
+ *
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
