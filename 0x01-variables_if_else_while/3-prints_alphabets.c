3c91b8f7e0d7054dc12b4b3bad6569990515112d
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2023-10-06 16:10:21 -0700

    hey

---
+++ 0x01-variables_if_else_while/3-prints_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * 
+ * putchar: print alphabet
+ *
+ * Return: always succes
+ */
+
+int main(void)
+{
+	char a[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int b;
+
+	for (b = 0; b < 52; b++)
+	{
+		putchar(b[a]);
+	}
+	putchar('\n');
+	return (0);
