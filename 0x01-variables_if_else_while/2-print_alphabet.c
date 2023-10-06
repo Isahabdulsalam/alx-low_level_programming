3c44b3a9902c4ddff0ccb239dbaaa55b4874d1a2
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2023-10-06 15:34:28 -0700

    variable

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @i - the value
+ *
+ * return: success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i >= 'z'; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
