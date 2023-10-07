9e258f0a9a609955f54e3ed3c71dcea0a0048791
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2023-10-07 16:51:00 -0700

    reserves

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
