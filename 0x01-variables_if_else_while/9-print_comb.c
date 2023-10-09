e9bf3167a1ffd01600291142cc5d1be8f21ec05c
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2023-10-09 14:21:03 -0700

    variables

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combination
+ *
+ * Return: always success
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
