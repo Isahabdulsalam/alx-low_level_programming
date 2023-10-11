86809c59609a4f382f89e94b81cff5f9309d73b6
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2023-10-10 18:12:26 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print a-z using putchar and prototype
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
