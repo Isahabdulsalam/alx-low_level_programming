30f860103e1bd11c3a791b43f3008e62df3270ed
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2023-10-19 16:07:43 -0700

    done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,19 +2,19 @@
 
 int print_sign(int n)
 {
-	if (n < 0)
+	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
+		return (1);
 	}
-	return (1);
-	else (n == 0)
+	else if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
+		return (0);
 	}
-	return (0);
-	else (n > 0);
+	else
 	{
-		putchar('-');
+		_putchar('-');
+		return (-1);
 	}
-	return (-1);
 }
