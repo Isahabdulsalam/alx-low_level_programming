4c7b41d3750ecbecfef4ca1074a2e4f31e9e6a8e
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2023-10-23 17:44:24 -0700

    pointer

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -15,7 +15,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	for (; a < b; a++)
 	{
-		dest[r] = src[r];
+		dest[a] = src[a];
 		n--;
 	}
 	return (dest);
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function to print chessbord
+ * @a: character
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int c;
+	int b;
+
+	for (c = 0; c < 8; c++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[c][b]);
+		}
+		_putchar('\n');
+	}
+}
