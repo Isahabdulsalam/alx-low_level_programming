7a0b9fe4458ef2e6172dfe4a11c88b0370afb76f
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2023-10-25 16:40:56 -0700

    recursion 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function to print string
+ * @s: the input
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
