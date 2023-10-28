2cb701259a6dd6d8f08591e6b5dc65c30a166206
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2023-10-27 17:05:36 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point that print number
+ * @argc: number of argument
+ * @argv: number of array
+ * Return: success p
+ */
+
+int main(argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
