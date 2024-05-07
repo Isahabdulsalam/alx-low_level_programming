48da67364ce1801152434546deb16b9f011b4d7f
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-05-07 16:19:41 -0700

    task

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stddef.h>
 
-
+int binary_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
-
+int recursive_search(int *array, size_t size, int value);
 #endif
